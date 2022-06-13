62898454cdec3c984a989267532d56cccd7ac3df
Felix Bole <felix.bole@yahoo.fr>
2022-06-13 17:41:53 +0200

    fix: Badge render for Jobready data

--- src/components/Views/SoftSkillsPage/JobReady.jsx
+++ src/components/Views/SoftSkillsPage/JobReady.jsx
@@ -11,7 +11,9 @@ import { useJsonFiles } from '../../Hooks/useJsonFiles'
 const JobReady = () => {
   const { t } = useI18n()
   const { jsonFiles } = useJsonFiles()
+  console.log(jsonFiles)
   const datas = jsonFiles.jobready.data?.data?.[0]?.fields || []
+  console.log(datas)
 
   return (
     <Accordion icon={JobReadyIcon} title={t("badges")}>
@@ -25,7 +27,7 @@ const JobReady = () => {
           datas.map(({ context: contexts, level, skill }, index) => (
             <Grid key={index} item xs={12} sm={4}>
               <Badge
-                title={skill}
+                title={skill.name || ""}
                 mainText={t("context") + ` : ${contexts.join(", ")}`}
                 subText={t("level") + ` : ${level}`}
               />
